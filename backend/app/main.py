a46866fa83ef696132dbdc37adba8e5611d232f5
zKarloz <karlozgutz@hotmail.com>
2026-09-21 00:05:34 -0500

    fix: corregir CORS para Vercel

--- backend/app/main.py
+++ backend/app/main.py
@@ -53,18 +53,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-    ],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://sistema-inteligente-grupo-1-frontend.vercel.app/",
+        "https://sistema-inteligente-grupo-1-frontend.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
