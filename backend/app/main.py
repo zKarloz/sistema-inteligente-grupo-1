8dcbe49e83140cad70bfacb9c75b1463644d3096
zKarloz <karlozgutz@hotmail.com>
2026-09-20 23:26:47 -0500

    fix: permitir frontend de Vercel en CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -59,6 +59,18 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://sistema-inteligente-grupo-1-frontend.vercel.app/",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 # Endpoint GET /Root
 @app.get("/")
 def root():
